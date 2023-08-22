38d8817bfc885b92e61357f8486c812a8790876a
Ali Aboussebaba <ali.aboussebaba@yandex.com>
2023-08-22 13:00:27 +0100

    Create default items when client created

--- Interfaces/Admin/Clients/Controllers/StoreClient.php
+++ Interfaces/Admin/Clients/Controllers/StoreClient.php
@@ -7,6 +7,7 @@ namespace Interfaces\Admin\Clients\Controllers;
 use App\Enums\RoutesEnum;
 use App\Http\Controllers\Controller;
 use Domain\Clients\Models\Client;
+use Domain\Clients\Models\Item;
 use Illuminate\Http\RedirectResponse;
 use Interfaces\Admin\Clients\Requests\StoreClientRequest;
 
@@ -14,7 +15,12 @@ class StoreClient extends Controller
 {
     public function __invoke(StoreClientRequest $request): RedirectResponse
     {
-        Client::create($request->safe(['name']));
+        $client = Client::create($request->safe(['name']));
+
+        // Create client default items
+        foreach (config('data.items') as $item) {
+            Item::create(array_merge($item, ['client_id' => $client->id]));
+        }
 
         return redirect()->route(RoutesEnum::ADMIN_INDEX_CLIENTS);
     }
