992950a7e7ffafcce6424a965a13a581cb9432b0
Ali Aboussebaba <ali.aboussebaba@yandex.com>
2023-08-22 13:07:14 +0100

    Add client refactoring

---
+++ resources/js/Shared/ButtonPrimary.vue
@@ -0,0 +1,13 @@
+<template>
+    <button type="submit" :disabled="disabled" class="inline-block px-6 py-2 border-2 border-blue-600 bg-blue-600 border-blue-600 text-white leading-tight font-bold rounded hover:bg-blue-500 focus:outline-none focus:ring-0 transition duration-150 ease-in-out">
+        <slot />
+    </button>
+</template>
+
+<script>
+export default {
+    props: {
+        disabled: Boolean,
+    },
+}
+</script>
---
+++ resources/js/Shared/FormGroup.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="form-group mb-6">
+        <label for="name" class="form-label inline-block mb-2 text-gray-700">
+            {{ label }}
+        </label>
+        <slot />
+        <div v-if="error" class="mt-2 font-semibold text-red-500">
+            {{ error }}
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        label: String,
+        error: String,
+    },
+}
+</script>
